97a759d88b0ae0fe690b8e5836ffacdfb40bb1ec
John Lightsey <john@nixnuts.net>
2018-02-06 15:05:33 -0600

    Reformat copyright blocks to match new source formatting.

--- src/PointerException.hpp
+++ src/PointerException.hpp
@@ -1,21 +1,22 @@
 /*
-    suPHP - (c)2002-2013 Sebastian Marsching <sebastian@marsching.com>
+  suPHP - (c)2002-2013 Sebastian Marsching <sebastian@marsching.com>
+          (c)2018 John Lightsey <john@nixnuts.net>
 
-    This file is part of suPHP.
+  This file is part of suPHP.
 
-    suPHP is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  suPHP is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    suPHP is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  suPHP is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with suPHP; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+  You should have received a copy of the GNU General Public License
+  along with suPHP; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
 #ifndef SUPHP_POINTEREXCEPTION_H
